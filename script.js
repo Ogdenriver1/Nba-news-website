f2bb8ace09ded19d193d7e14011e485434224b49
Ogdenriver1 <jordan.pouladian@gmail.com>
2025-04-26 08:19:49 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const newsArea = document.getElementById('news-area');
 
   async function getTheNews() {
-    const response = await fetch('/.netlify/functions/news'); // Ask our Netlify code for the news
+    const response = await fetch('public/nba-news.json'); // Ask our Netlify code for the news
     const data = await response.json(); // Get the news in a readable format
-    displayTheNews(data.news); // Show the news on the webpage
+    console.log("Fetched news data:", data);
+    displayTheNews(data); // Show the news on the webpage
   }
 
   function displayTheNews(newsList) {
     newsList.forEach(newsItem => {
       const newsElement = document.createElement('div');
-      newsElement.innerHTML = `<h3>${newsItem.title}</h3><p>${newsItem.summary}</p>`;
+      newsElement.innerHTML = `<h3>${newsItem.Title}</h3><p>${newsItem.Content}</p>`;
       newsArea.appendChild(newsElement);
     });
   }
